b0be88fb0b8ac2510b5f21cf92a0e1cd0b4a0b90
autisticwrong <43218868+autisticwrong@users.noreply.github.com>
2020-10-27 13:35:34 -0400

    Update hanime.py

--- scraper/hanime.py
+++ scraper/hanime.py
@@ -1,4 +1,5 @@
 import os
+import time
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
@@ -13,14 +14,14 @@ config = {
   "target_path": './images/', 
   "tags": 
     {
-    "SFW": { "enabled": False, "divIndex": 1 },
-    "NSFW": { "enabled": True, "divIndex": 2 },
-    "Furry": { "enabled": False, "divIndex": 3 },
-    "Futa": { "enabled": False, "divIndex": 4 },
-    "Yaoi": { "enabled": False, "divIndex": 5 },
-    "Yuri": { "enabled": True, "divIndex": 6 },
-    "Trap": { "enabled": False, "divIndex": 7 },
-    "3D": { "enabled": False, "divIndex": 8 },
+    "SFW": {"enabled": False, "divIndex": 1},
+    "NSFW": {"enabled": True, "divIndex": 2},
+    "Furry": {"enabled": False, "divIndex": 3},
+    "Futa": {"enabled": False, "divIndex": 4},
+    "Yaoi": {"enabled": False, "divIndex": 5},
+    "Yuri": {"enabled": True, "divIndex": 6},
+    "Trap": {"enabled": False, "divIndex": 7},
+    "3D": {"enabled": False, "divIndex": 8},
   }
 }
 
@@ -35,11 +36,19 @@ class Hanime():
     if not os.path.exists(self.config["target_path"]):
       os.mkdir(self.config["target_path"])
     os.chmod(self.config["target_path"], 0o777)
-    pages = self.pages()
     
-    for i in range(pages):
-      if i < pages:
-        self.scrape()
+    pages = self.pages()
+    self.current_page = 1
+    
+    self.driver.find_element_by_xpath("""//*[@id="app"]/div[4]/main/div/div/div/div[3]/div[1]/div""").click() #FUCK SFW
+    time.sleep(1)
+    
+    while self.current_page <= pages:
+      self.scrape()
+      nextPage = self.driver.find_element_by_xpath("""//*[@id="app"]/div[4]/main/div/div/div/div[4]/button[3]/div""")
+      nextPage.click()
+      self.current_page += 1
+
     print(len(self.links))
     
   def pages(self):
@@ -49,7 +58,7 @@ class Hanime():
   def scrape(self):
     for i in range(1, 100):
       try:
-        content = self.driver.find_element_by_xpath("""//*[@id="app"]/div[4]/main/div/div/div/div[5]/a[{0}]""".format(i)).get_attribute("href")
+        content = self.driver.find_element_by_xpath("""//*[@id="app"]/div[4]/main/div/div/div/div[5]/a[1]""").get_attribute("href")
         print(content)
         self.links.append(content)
     
