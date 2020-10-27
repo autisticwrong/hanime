b74e9fb45ee37a58539a3bdb03790f98c0f4f17e
autisticwrong <43218868+autisticwrong@users.noreply.github.com>
2020-10-27 14:55:51 -0400

    Create discordbot.py

---
+++ tests/discordbot.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+from scraper.hanime import Hanime
+
+client = commands.Bot(command_prefix="s.")
+client.remove_command("help")
+
+@client.event
+async def on_ready():
+  print("Online!")
+
+@client.command()
+async def test(ctx):
+  hanime = Hanime(config)
+  hanime.run()
+  for link in hanime.links:
+    await ctx.send(link)
+
+client.run("token")
