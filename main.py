f4b63aadffcd495e11bc6e003ddf5008ecbb01f2
autisticwrong <43218868+autisticwrong@users.noreply.github.com>
2020-11-18 08:58:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,10 @@
-import discord
-from discord.ext import commands
-from scraper.hanime import Hanime
-
-client = commands.Bot(command_prefix="s.")
-client.remove_command("help")
+from scraper.scrape import Hanime
 
 config = {
   "target_path": './images/',
   "driver_path": "chromedriver.exe"
 }
 
-@client.event
-async def on_ready():
-  print("Online!")
-
-@client.command()
-async def poon(ctx):
-  hanime = Hanime(config)
-  hanime.run()
-  for link in hanime.links:
-    await ctx.send(link)
-
-client.run("token here")
+hanime = Hanime(config)
+hanime.run()
+hanime.download()
